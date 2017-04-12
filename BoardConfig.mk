8225308b62c005087fd1ec76b4ce6f90b84cfa45
lihonglinglory <lihonglin10086@163.com>
2017-04-12 17:45:22 +0800

    Add proprietary-files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-USE_camERA_STUB := true
+USE_CAMERA_STUB := true
 
 LOCAL_PATH := device/honor/cam
 
